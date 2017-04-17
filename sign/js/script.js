a3f7d296aea787c9b9ae94a0262f98cba89ec4f9
Bhavika Bhatia <bhatb280@newschool.edu>
2017-04-17 18:55:27 -0400

    sign

---
+++ sign/js/script.js
@@ -0,0 +1,75 @@
+$(document).ready(function() {
+ 
+ $('#N').hover(function(){
+  $('#Ntext').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#Ntext').hide();
+  console.log("hello");
+ });
+
+
+
+ $('#E').hover(function(){
+  $('#Etext').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#Etext').hide();
+  console.log("hello");
+ });
+
+
+
+ $('#W').hover(function(){
+  $('#Wtext').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#Wtext').hide();
+  console.log("hello");
+ });
+
+
+
+$('#MU').hover(function(){
+  $('#MUtext').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#MUtext').hide();
+  console.log("hello");
+ }); 
+
+
+
+$('#SE').hover(function(){
+  $('#SEtext').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#SEtext').hide();
+  console.log("hello");
+ });
+
+
+
+$('#UM').hover(function(){
+  $('#UMtext').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#UMtext').hide();
+  console.log("hello");
+ });
+
+});
+
+
