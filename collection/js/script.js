f4a69d12ceb3d5a6b7d0f2be70d08f27e139cda5
bhatiabhavika <bhatb280@newschool.edu>
2017-05-11 03:02:53 -0400

    Add files via upload

---
+++ collection/js/script.js
@@ -0,0 +1,326 @@
+$(document).ready(function() {
+ 
+ $('#tube-colourpop').hover(function(){
+  $('#colourpop').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#colourpop').hide();
+  console.log("hello");
+ });
+
+
+
+ $('#pencil-colourpop').hover(function(){
+  $('#colourpop').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#colourpop').hide();
+  console.log("hello");
+ });
+
+
+ $('#tub-colourpop').hover(function(){
+  $('#colourpop').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#colourpop').hide();
+  console.log("hello");
+ });
+
+
+
+$('#tube-nyx').hover(function(){
+  $('#nyx').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#nyx').hide();
+  console.log("hello");
+ }); 
+
+
+
+$('#pencil-nyx').hover(function(){
+  $('#nyx').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#nyx').hide();
+  console.log("hello");
+ }); 
+
+
+
+$('#tub-nyx').hover(function(){
+  $('#nyx').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#nyx').hide();
+  console.log("hello");
+ }); 
+
+
+$('#palette-nyx').hover(function(){
+  $('#nyx').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#nyx').hide();
+  console.log("hello");
+ }); 
+
+
+$('#tube-mac').hover(function(){
+  $('#mac').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#mac').hide();
+  console.log("hello");
+ }); 
+
+
+$('#pencil-mac').hover(function(){
+  $('#mac').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#mac').hide();
+  console.log("hello");
+ }); 
+
+
+$('#bottle-mac').hover(function(){
+  $('#mac').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#mac').hide();
+  console.log("hello");
+ }); 
+
+
+$('#pan-mac').hover(function(){
+  $('#mac').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#mac').hide();
+  console.log("hello");
+ }); 
+
+
+$('#tube-flormar').hover(function(){
+  $('#flormar').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#flormar').hide();
+  console.log("hello");
+ }); 
+
+
+$('#tube-mikyaji').hover(function(){
+  $('#mikyaji').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#mikyaji').hide();
+  console.log("hello");
+ });
+
+
+$('#pencil-nars').hover(function(){
+  $('#nars').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#nars').hide();
+  console.log("hello");
+ });
+
+
+$('#bottle-nars').hover(function(){
+  $('#nars').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#nars').hide();
+  console.log("hello");
+ });
+
+
+$('#pencil-lagirl').hover(function(){
+  $('#lagirl').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#lagirl').hide();
+  console.log("hello");
+ });
+
+
+$('#tube-lagirl').hover(function(){
+  $('#lagirl').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#lagirl').hide();
+  console.log("hello");
+ });
+
+
+$('#palette-urbandecay').hover(function(){
+  $('#urbandecay').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#urbandecay').hide();
+  console.log("hello");
+ });
+
+
+$('#pan-abh').hover(function(){
+  $('#abh').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#abh').hide();
+  console.log("hello");
+ });
+
+
+$('#tub-elf').hover(function(){
+  $('#elf').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#elf').hide();
+  console.log("hello");
+ });
+
+
+$('#palette-morphe').hover(function(){
+  $('#morphe').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#morphe').hide();
+  console.log("hello");
+ });
+
+
+$('#bottle-maybelline').hover(function(){
+  $('#maybelline').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#maybelline').hide();
+  console.log("hello");
+ });
+
+
+$('#box-benefit').hover(function(){
+  $('#benefit').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#benefit').hide();
+  console.log("hello");
+ });
+
+
+$('#tube-benefit').hover(function(){
+  $('#benefit').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#benefit').hide();
+  console.log("hello");
+ });
+
+
+$('#tub-bennye').hover(function(){
+  $('#bennye').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#bennye').hide();
+  console.log("hello");
+ });
+
+
+$('#pencil-marcjacobs').hover(function(){
+  $('#marcjacobs').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#marcjacobs').hide();
+  console.log("hello");
+ });
+
+
+$('#pencil-sephora').hover(function(){
+  $('#sephora').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#sephora').hide();
+  console.log("hello");
+ });
+
+
+$('#tube-loreal').hover(function(){
+  $('#loreal').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#loreal').hide();
+  console.log("hello");
+ });
+
+
+$('#pencil-katvond').hover(function(){
+  $('#katvond').show();
+  console.log("hello");
+ },
+
+ function(){
+  $('#katvond').hide();
+  console.log("hello");
+ });
+
+
+});
